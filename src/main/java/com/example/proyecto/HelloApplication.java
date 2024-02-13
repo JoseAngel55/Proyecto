c606b672d8299cef25d83107a7fcd26ecf832cb9
Angel Navarro <navarrojoseangel749@gmail.com>
2024-02-13 17:21:56 -0600

    Estilos

--- src/main/java/com/example/proyecto/HelloApplication.java
+++ src/main/java/com/example/proyecto/HelloApplication.java
@@ -4,22 +4,54 @@ import com.example.proyecto.vistas.Calculadora;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private MenuBar mnbPrincipal;
+    private Menu menParcial1, menParcial2, menSalir;
+    private MenuItem mitCalculadora, mitSalir;
+    private BorderPane bdpPanel;
+
     @Override
     public void start(Stage stage) throws IOException {
+        CrearMenu();
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        Scene scene = new Scene(new BorderPane());
+        bdpPanel = new BorderPane();
+        bdpPanel.setTop(mnbPrincipal);
+        Scene scene = new Scene(bdpPanel);
+        scene.getStylesheets().add(getClass().getResource("/Estilos/main.css").toString());
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
 
-        new Calculadora();
+        //new Calculadora();
+    }
+
+    private void CrearMenu() {
+        /* Menu primer parcial*/
+        mitCalculadora = new MenuItem("Calculadora");
+        menParcial1 = new Menu("Primer Parcial");
+        menParcial1.getItems().addAll(mitCalculadora);
+
+        /* Menu Segundo parcial*/
+        menParcial2 = new Menu("Segundo Parcial");
+
+        /* Menu salir*/
+        mitSalir = new MenuItem("Salir");
+        menSalir = new Menu("Salir");
+        menSalir.getItems().add(mitSalir);
+        menSalir.setOnAction(event -> System.exit(0));
+
+        mnbPrincipal = new MenuBar();
+        mnbPrincipal.getMenus().addAll(menParcial1, menParcial2, menSalir);
     }
 
     public static void main(String[] args) {
